7609bfe3644eded164573de2afe3ea83eeb99e6d
Ignacio Rossi <irossi@medallia.com>
2019-07-05 14:08:41 -0300

    Remove python2 support

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'Click>=6.0', 
+    'requests>=2.21.0', 
+    'beautifulsoup4>=4.7.1',
+]
 
 setup_requirements = [ ]
 
@@ -25,8 +29,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
