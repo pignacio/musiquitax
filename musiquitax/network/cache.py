9afaef72c048d39018d7ba5e042576b5b63e1dc1
Ignacio Rossi <irossi@medallia.com>
2019-07-06 13:58:40 -0300

    Reload cached pages once per week

--- musiquitax/network/cache.py
+++ musiquitax/network/cache.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import sqlite3
 import time
@@ -7,6 +8,10 @@ from ..network import UrlFetcher
 
 logger = logging.getLogger(__name__)
 
+_ONE_WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000
+
+DatedContents = collections.namedtuple("DatedContent", ["contents", "date"])
+
 
 class CachedFetcher(UrlFetcher):
     def __init__(self, delegate: UrlFetcher, db_file: str = ".fetcher_cache"):
@@ -15,23 +20,24 @@ class CachedFetcher(UrlFetcher):
         self._sync_table()
 
     def fetch(self, url: str) -> str:
-        cached = self._get_page(url)
-        if cached is None:
+        cached = self._get_page_and_date(url)
+        if cached is None or self._should_reload(cached):
             contents = self.__delegate.fetch(url)
             self._save_page(url, contents)
             return contents
         else:
-            return cached
+            return cached.contents
 
-    def _get_page(self, url: str) -> Optional[str]:
+    def _get_page_and_date(self, url: str) -> Optional[DatedContents]:
         with self.__conn:
-            res = self.__conn.execute("SELECT contents FROM fetcher_cache WHERE url = ?", (url,)).fetchone()
+            res = self.__conn.execute("SELECT contents, date FROM fetcher_cache WHERE url = ?", (url,)).fetchone()
             if res is not None:
-                return res[0]
+                return DatedContents(res[0], res[1])
         return None
 
     def _save_page(self, url: str, contents: str):
         with self.__conn:
+            self.__conn.execute("DELETE FROM fetcher_cache WHERE url = ?", (url,))
             self.__conn.execute("INSERT INTO fetcher_cache VALUES (?, ?, ?)", (url, contents, self._current_time()))
 
     def _sync_table(self):
@@ -44,6 +50,9 @@ class CachedFetcher(UrlFetcher):
                 "  date BIGINT NOT NULL"
                 ")"
             )
+
+    def _should_reload(self, cached: DatedContents) -> bool:
+        return self._current_time() - cached.date > _ONE_WEEK_IN_MS
 
     @staticmethod
     def _current_time() -> int:
